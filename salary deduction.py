ea78b8a53b5ca2ded5b18ed1967227a9e1307381
apprealmei <lumbab.april06@gmail.com>
2025-03-06 17:08:27 +0800

    Convert to modular functions-April Lumbab

--- salary deduction.py
+++ salary deduction.py
@@ -1,9 +1,30 @@
-def compute_deductions(salary):
-    sss_contribution = 1200
-    philhealth_contribution = (salary * 0.05) / 2
-    pagibig_contribution = 100
-    tax_deduction = 1875
 
+def calculate_sss_contribution(salary):
+    sss_contribution = 1200  
+    return sss_contribution
+
+def calculate_philhealth_contribution(salary):
+    philhealth_contribution = (salary * 0.05) / 2 
+    return philhealth_contribution
+
+def calculate_pagibig_contribution():
+    pagibig_contribution = 100  
+    return pagibig_contribution
+
+
+def calculate_tax_deduction():
+    tax_deduction = 1875  # Assuming fixed value for simplicity
+    return tax_deduction
+
+
+def compute_deductions(salary):
+    
+    sss_contribution = calculate_sss_contribution(salary)
+    philhealth_contribution = calculate_philhealth_contribution(salary)
+    pagibig_contribution = calculate_pagibig_contribution()
+    tax_deduction = calculate_tax_deduction()
+
+    
     total_deductions = sss_contribution + philhealth_contribution + pagibig_contribution + tax_deduction
     net_salary = salary - total_deductions
 
